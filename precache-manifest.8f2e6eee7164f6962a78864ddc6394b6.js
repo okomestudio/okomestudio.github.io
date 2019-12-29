7fc735ec4534d165c98afae935d936bb92b35ec8
Taro Sato <okomestudio@gmail.com>
2019-12-29 01:07:17 -0800

    Rewrite with React

---
+++ precache-manifest.8f2e6eee7164f6962a78864ddc6394b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618575d28cd62b30dfa950a5a5f743b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63451be739b54422570e",
+    "url": "/static/css/main.982a08f3.chunk.css"
+  },
+  {
+    "revision": "4c8a1d63ad7783858c76",
+    "url": "/static/js/2.c8c08786.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.c8c08786.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63451be739b54422570e",
+    "url": "/static/js/main.d5c9d10f.chunk.js"
+  },
+  {
+    "revision": "bc3e789ad8caa7811352",
+    "url": "/static/js/runtime-main.e4455852.js"
+  }
+]);
