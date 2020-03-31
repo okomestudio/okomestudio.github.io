6a12ffdb737addc74481654585c3a8aed1d13425
Taro Sato <okomestudio@gmail.com>
2020-03-31 10:45:21 -0700

    Version 202003.10

--- biboroku/static/pjump.js
+++ biboroku/static/pjump.js
@@ -1,11 +1,16 @@
 /* The following code is to map old param-based WordPress URLs to the current path-based URLs. */
 
 var urlmap = {
+  291: "2009/03/formatting-a-usb-drive-for-use-with-debian",
   636: "2010/03/installing-hotpants-5-1-10-on-mac-os-x-leopard",
+  719: "2010/03/installing-fftw-on-mac-os-x-snow-leopard",
   824: "2010/03/installing-sextractor-on-mac-os-x-snow-leopard",
   986: "2010/06/testing-if-a-point-is-inside-a-polygon-in-python",
   1131: "2010/11/installing-f-lux-on-debian-squeeze",
+  1386: "2010/12/installing-debian-squeeze-from-hard-disk",
   1834: "2012/09/using-japanese-on-debian-wheezy",
+  1986: "2014/02/installing-debian-wheezy-from-a-usb-stick",
+  2065: "2014/04/near-duplicate-detection-using-minhash-background",
   2292: "2014/04/pca-and-biplot-using-python",
   2357: "2014/05/brand-positioning-by-correspondence-analysis",
   2375: "2014/05/interpreting-ab-test-using-python",
